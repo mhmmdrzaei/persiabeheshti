6bea62beece9ba0cecb25197a0e3e7efde764013
Mohammad <mhmmdrzaei@gmail.com>
2021-01-27 11:00:32 -0500

    final touches

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<?php // Load Meta ?>
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
